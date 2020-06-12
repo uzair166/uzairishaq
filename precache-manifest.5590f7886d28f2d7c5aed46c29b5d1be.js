be60ba96f17f3a925dff7ebc4c839a14fd9f70ca
Uzair Ishaq <uzair.i@hotmail.co.uk>
2020-06-12 02:15:41 +0100

    Updates

---
+++ precache-manifest.5590f7886d28f2d7c5aed46c29b5d1be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ce08d4db1089c515ff89bb1314152e",
+    "url": "/uzairishaq/index.html"
+  },
+  {
+    "revision": "977321904447ef8c148a",
+    "url": "/uzairishaq/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a3afffc0f5fac0c08a81",
+    "url": "/uzairishaq/static/css/main.80d7f8bc.chunk.css"
+  },
+  {
+    "revision": "977321904447ef8c148a",
+    "url": "/uzairishaq/static/js/2.ec56ed15.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/uzairishaq/static/js/2.ec56ed15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3afffc0f5fac0c08a81",
+    "url": "/uzairishaq/static/js/main.6425d142.chunk.js"
+  },
+  {
+    "revision": "f4599d6681f2cd9985ef",
+    "url": "/uzairishaq/static/js/runtime-main.002f852c.js"
+  }
+]);
